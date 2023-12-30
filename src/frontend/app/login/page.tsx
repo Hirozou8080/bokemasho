edbdce88df6a370a6c22057e7025e15fad027a39
h_kinjo <h.kinjo.11.8@gmail.com>
2023-12-30 15:45:16 +0900

    login page create

---
+++ src/frontend/app/login/page.tsx
@@ -0,0 +1,14 @@
+import React, { useEffect, useState } from 'react';
+// import axios from `'axios';
+import Login from '@/app/components/Login';
+
+const Page = () => {
+
+    return (
+        <div>
+            <Login />
+        </div>
+    );
+}
+
+export default Page;
