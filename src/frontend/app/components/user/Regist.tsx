003ba2b454301ee87a4fd01d227d87e2b3f3a0e2
h_kinjo <h.kinjo.11.8@gmail.com>
2024-01-02 23:48:59 +0900

    fix

--- src/frontend/app/components/user/Regist.tsx
+++ src/frontend/app/components/user/Regist.tsx
@@ -5,7 +5,7 @@ const Regist = () => {
       <input
           type="text"
           className='bg-gray-200 appearance-none border-2 border-gray-200 rounded w-full py-2 px-4 text-gray-700 leading-tight focus:outline-none focus:bg-white focus:border-purple-500 m-3 max-w-sm'
-          placeholder='user Name'
+          placeholder='email'
           onChange={(e) => {
             console.log(e.target.value);
             // setEmail(e.target.value);
